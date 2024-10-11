45edb9b7960e11a8ea580dd3290a059d60deb765
Manove Kumar <kumarmanove2003@gmail.com>
2024-10-12 00:30:46 +0500

    first commit 2

--- src/pages/LoginSystem.js
+++ src/pages/LoginSystem.js
@@ -74,7 +74,7 @@ const LoginSystem = ({ toggleView }) => {
       // Optionally, you can call an API to update the role here if needed
       updateRoleOnLogin();
 
-      navigate("/welcome"); // Redirect to the welcome page
+      navigate(`/welcome?token=${token}&role=${user.role}`); // Redirect to the welcome page
     }
   }, [navigate]);
 
